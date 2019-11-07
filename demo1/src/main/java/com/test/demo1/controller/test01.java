1d3bcf521295cc646e36e102b432ffa040fde10c
jc <2607339003@qq.com>
2019-11-07 15:39:41 +0800

    dev222

--- demo1/src/main/java/com/test/demo1/controller/test01.java
+++ demo1/src/main/java/com/test/demo1/controller/test01.java
@@ -5,6 +5,7 @@ public class test01 {
     public static void main(String[] args) {
         System.out.println(12);
         System.out.println("dev2");
+        saydev2();
     }
 
     public static void saydev2(){
