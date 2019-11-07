446486661fd543de0b651e289b47ba4c2b766843
jc <2607339003@qq.com>
2019-11-07 15:21:52 +0800

    Revert "dev1"
    This reverts commit 7ca8986c

--- demo1/src/main/java/com/test/demo1/controller/test02.java
+++ demo1/src/main/java/com/test/demo1/controller/test02.java
@@ -3,10 +3,6 @@ package com.test.demo1.controller;
 public class test02 {
     public static void main(String[] args) {
         System.out.println(21);
-        say();
-    }
-
-    public static void say(){
-        System.out.println(123);
+        System.out.println(44);
     }
 }
