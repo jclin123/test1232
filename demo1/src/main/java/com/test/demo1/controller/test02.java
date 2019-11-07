7ca8986c280d1817e3119be2e61535b3dc46ccb2
jc <2607339003@qq.com>
2019-11-07 15:16:01 +0800

    dev1

--- demo1/src/main/java/com/test/demo1/controller/test02.java
+++ demo1/src/main/java/com/test/demo1/controller/test02.java
@@ -3,5 +3,10 @@ package com.test.demo1.controller;
 public class test02 {
     public static void main(String[] args) {
         System.out.println(21);
+        say();
+    }
+
+    public static void say(){
+        System.out.println(123);
     }
 }
