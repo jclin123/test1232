d4c8d73d1abeb45dba6c0d0646cf1f5873c5acdb
jc <2607339003@qq.com>
2019-11-07 15:30:55 +0800

    master

--- demo1/src/main/java/com/test/demo1/controller/test02.java
+++ demo1/src/main/java/com/test/demo1/controller/test02.java
@@ -4,5 +4,9 @@ public class test02 {
     public static void main(String[] args) {
         System.out.println(21);
         System.out.println(44);
+        say();
+    }
+    public static void say(){
+        System.out.println("hello");
     }
 }
