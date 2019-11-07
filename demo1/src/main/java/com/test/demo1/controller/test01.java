543ac35a23bd3db5b59352215c6061f870cc22f3
jc <2607339003@qq.com>
2019-11-07 15:45:26 +0800

    dev2回退

--- demo1/src/main/java/com/test/demo1/controller/test01.java
+++ demo1/src/main/java/com/test/demo1/controller/test01.java
@@ -5,6 +5,10 @@ public class test01 {
     public static void main(String[] args) {
         System.out.println(12);
         System.out.println("dev2");
-        //saydev2();
+        saydev2();
+    }
+
+    public static void saydev2(){
+        System.out.println("hello dev2");
     }
 }
