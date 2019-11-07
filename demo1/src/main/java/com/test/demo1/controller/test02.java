ae4fcef649ab68db8105a388e522a6da79d22337
jc <2607339003@qq.com>
2019-11-07 15:10:36 +0800

    dev1

--- demo1/src/main/java/com/test/demo1/controller/test02.java
+++ demo1/src/main/java/com/test/demo1/controller/test02.java
@@ -1,8 +1,7 @@
 package com.test.demo1.controller;
 
-public class test01 {
-
+public class test02 {
     public static void main(String[] args) {
-        System.out.println(12);
+        System.out.println(21);
     }
 }
