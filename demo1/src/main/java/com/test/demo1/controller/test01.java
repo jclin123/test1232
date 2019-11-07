e717967d8c26b170323a5d44764b527cc7156905
jc <2607339003@qq.com>
2019-11-07 15:36:18 +0800

    Revert "dev1"
    This reverts commit 7ca8986c280d1817e3119be2e61535b3dc46ccb2.

--- demo1/src/main/java/com/test/demo1/controller/test01.java
+++ demo1/src/main/java/com/test/demo1/controller/test01.java
@@ -6,4 +6,8 @@ public class test01 {
         System.out.println(12);
         System.out.println("dev2");
     }
+
+    public static void saydev2(){
+        System.out.println("hello dev2");
+    }
 }
