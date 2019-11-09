51eccf8fc8200b3b8807433279b9db9e425e7185
jc <2607339003@qq.com>
2019-11-09 18:23:24 +0800

    dev2解决冲突

--- demo1/src/main/java/com/test/demo1/controller/test01.java
+++ demo1/src/main/java/com/test/demo1/controller/test01.java
@@ -5,7 +5,9 @@ public class test01 {
     public static void main(String[] args) {
         System.out.println(12);
         System.out.println("dev2");
+        System.out.println("dev22");
         saydev2();
+        System.out.println("dev22");
     }
 
     public static void saydev2(){
